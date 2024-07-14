cf8533c4e179c0a5399e2fda36a48ecaab92136e
Anastasiia Prokofieva <prokofieva.nastia42342@gmail.com>
2024-07-14 11:33:30 +0100

    Second commit

--- L3/HW3_3.py
+++ L3/HW3_3.py
@@ -1,11 +1,13 @@
-lst = [1, 2, 3, 4, 5, 6]
+# lst = [1, 2, 3, 4, 5, 6]
 # lst = [1, 2, 3]
 # lst = [1, 2, 3, 4, 5]
 # lst = [1]
-# lst = []
+lst = []
 print(lst)
 
-if not len(lst) % 2:
+if len(lst) == 0:
+    lst = [lst[:0], lst[0:]]
+elif not len(lst) % 2:
     lst_1 = lst[:int(len(lst) / 2)]
     lst_2 = lst[int(len(lst) / 2):]
     lst = [lst_1, lst_2]
