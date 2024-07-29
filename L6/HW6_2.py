fd20b6951d30db00af9b36f9514dfc5f12a3ea29
Anastasiia Prokofieva <prokofieva.nastia42342@gmail.com>
2024-07-29 08:17:35 +0100

    Second commit

--- L6/HW6_2.py
+++ L6/HW6_2.py
@@ -3,7 +3,8 @@ if 0 <= n < 8640000:
     all_mins, sec = divmod(n, 60)
     all_hours, mins = divmod(all_mins, 60)
     days, hours = divmod(all_hours, 24)
-    data = f'{days} days, {str(hours).zfill(2)}:{str(mins).zfill(2)}:{str(sec).zfill(2)}'
+    data = f'{days} днів, {str(hours).zfill(2)}:{str(mins).zfill(2)}:{str(sec).zfill(2)}'
     print(data)
 else:
     print('out of range')
+
